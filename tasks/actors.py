23e4583630d7c401c0024be986504ea61c0a2c8f
Dimitris Tsirmpas <tsirbasdim@gmail.com>
2024-06-30 13:43:01 +0300

    fix prompt programmatic structure

--- tasks/actors.py
+++ tasks/actors.py
@@ -1,3 +1,4 @@
+import llama_cpp
 import tasks.models
 import abc
 import typing
@@ -32,24 +33,20 @@ class LlmActor(Actor):
 
     
     def _system_prompt(self) -> dict:
-        prompt = f"""
-        You are {self.name} a {",".join(self.attributes)} user. {self.context} {self.instructions}.'
-        """ 
+        prompt = f"You are {self.name} a {",".join(self.attributes)} user. {self.context} {self.instructions}." 
         return {"role": "system", "content": prompt}
     
-    def _message_prompt(self, message: str) -> dict:
-        return {"role": "user", "content": message}
+    def _message_prompt(self, history: list[str]) -> dict:
+        return {
+                "role": "user",
+                "content": "\n".join(history) + f"\n{self.get_name()}:"
+              }
 
     @typing.final
     def speak(self, history: list[str]) -> str:
         system_prompt = self._system_prompt()
-        messages = []
-        for message in history:
-            message_prompt = self._message_prompt(message)
-            messages.append((system_prompt, message_prompt))
-        
-        response = self.model.prompt(messages)
-
+        message_prompt = self._message_prompt(history)
+        response = self.model.prompt([system_prompt, message_prompt])
         return response
 
     @typing.final

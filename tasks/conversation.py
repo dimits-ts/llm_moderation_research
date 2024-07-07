5fac21ee502f02a64ada0c35628b671412ce34f5
Dimitris Tsirmpas <tsirbasdim@gmail.com>
2024-07-07 12:37:56 +0300

    refactor conversation manager to conversation
    now supports moderator intervention at any point in the conversation

--- tasks/conversation.py
+++ tasks/conversation.py
@@ -1,49 +1,51 @@
-import tasks.actors
-import pandas as pd
-
 import collections
-import time
 import textwrap
+import time
+
+import tasks.actors
 
 
-class ConversationManager:
+class Conversation:
 
-    def __init__(self, actors: list[tasks.actors.Actor], history_len: int=5, conv_len: int=5) -> None:
-        self.actors = actors
-        self.history_len = history_len
+    def __init__(
+        self,
+        users: list[tasks.actors.Actor],
+        moderator: tasks.actors.Actor | None = None,
+        history_context_len: int = 5,
+        conv_len: int = 5,
+    ) -> None:
+        self.users = users
+        self.moderator = moderator
         self.conv_len = conv_len
+        # unique id for each conversation, generated for persistence purposes
+        self.id = str(time.time())
 
-    def begin_conversation(self, verbose: bool=True) -> pd.DataFrame:
-        # unique id for each conversation, generated for persisence purposes
-        conv_id = str(time.time())
+        self.conv_logs = []
+        # keep a limited context of the conversation to feed to the models
+        self.ctx_history = collections.deque(maxlen=history_context_len)
 
-        total_history = []
-        # counter to determine the ordering of the responses in the dataframe
-        conv_counter = 0
-        # keep a limited context of the conversation
-        history = collections.deque(maxlen=self.history_len)
+    def begin_conversation(self, verbose: bool = True) -> None:
+        if len(self.conv_logs) != 0:
+            raise RuntimeError("This conversation has already been concluded, create a new Conversation object.")
 
         for _ in range(self.conv_len):
-            for actor in self.actors:
-                conv_counter += 1
+            for user in self.users:
+                self.actor_turn(user, verbose)
+                if self.moderator is not None:
+                    self.actor_turn(self.moderator, verbose)
 
-                res = actor.speak(list(history))
+    def actor_turn(self, actor: tasks.actors.Actor, verbose: bool) -> None:
+        res = actor.speak(list(self.ctx_history))
+        self.conv_logs.append((self.id, actor.get_name(), res))
 
-                if len(res.strip()) != 0: 
-                    total_history.append((conv_id, conv_counter, actor.get_name(), res))
-                    
-                    # append name of actor to his response
-                    # "user x posted" important for the model to not confuse it with the prompt
-                    wrapped_res = textwrap.fill(res, 70)
-                    named_res = f"User {actor.get_name()} posted:\n{wrapped_res}"
-                    history.append(named_res)
+        if len(res.strip()) != 0:
+            # append name of actor to his response
+            # "user x posted" important for the model to not confuse it with the prompt
+            wrapped_res = textwrap.fill(res, 70)
+            formatted_res = f"User {actor.get_name()} posted:\n{wrapped_res}"
+        else:
+            formatted_res = f"<{actor.get_name()} said nothing>"
 
-                    if verbose:
-                        print(named_res)
-                else:
-                    if verbose:
-                        print(f"<{actor.get_name()} said nothing>")
-
-        return pd.DataFrame(total_history, 
-                            columns=["conv_id", "response_order", "actor_name", "content"])
-
+        if verbose:
+            print(formatted_res)
+        self.ctx_history.append(formatted_res)

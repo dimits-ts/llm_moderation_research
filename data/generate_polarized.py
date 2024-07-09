ed6e13d929191576b4b6d3e491bc986ec7f54f47
Dimitris Tsirmpas <tsirbasdim@gmail.com>
2024-07-09 14:03:30 +0300

    Add more test cases

---
+++ data/generate_polarized.py
@@ -0,0 +1,4 @@
+from tasks.conversation import LLMConvData
+
+
+o1 = LLMConvData(context="")
--- tests/test_conversation.py
+++ tests/test_conversation.py
@@ -3,11 +3,32 @@ from tasks.conversation import LLMConvData
 
 
 class TestLLMConvData(TestCase):
+
+    def test_constructor_assertions(self):
+        self.assertRaises(AssertionError, lambda: LLMConvData(
+            context="You are in an online chatroom. You see the following post on a social media site:"
+                    "'Homosexuality, transgender sex all are deviations from normal human behavior "
+                    "as well.'",
+            actor_names=["Steve2001", "GeorgeBush78"],
+            actor_attributes=[
+                ["African American"],
+            ],
+            actor_instructions="Write a comment responding to the post. Insist on your opinion"
+                               " unless convinced by other users."
+                               " Try to follow the instructions of the moderator, if he intervenes."
+                               " You are allowed to act frustrated or aggressively if provoked."))
+
     def test_from_json_file(self):
         data = LLMConvData.from_json_file("output/test1.json")
         assert len(data.actor_names) != 0
         assert data.moderator_name is not None
         print(data)
+
+        data2 = LLMConvData.from_json_file("output/test2.json")
+        assert len(data2.actor_names) != 0
+        assert data2.moderator_name is None
+
+        self.assertRaises(AssertionError, lambda: LLMConvData.from_json_file("output/test_invalid.json"))
 
     def test_to_json_file(self):
         data = LLMConvData(context="You are in an online chatroom. You see the following post on a social media site:"
@@ -21,11 +42,25 @@ class TestLLMConvData(TestCase):
                            actor_instructions="Write a comment responding to the post. Insist on your opinion"
                                               " unless convinced by other users."
                                               " Try to follow the instructions of the moderator, if he intervenes."
-                                              " You are allowed to act frustrated or aggresively if provoked.",
+                                              " You are allowed to act frustrated or aggressively if provoked.",
                            moderator_name="moderator01",
                            moderator_attributes=["just", "strict"],
                            moderator_instructions="Intervene if one user dominates or veers off-topic. "
                                                   "Respond only if necessary. "
-                                                  "Write '<No response>' if intervention is unecessary."
-                                                  " Be firm and threaten to displine non-cooperating users.")
+                                                  "Write '<No response>' if intervention is necessary."
+                                                  " Be firm and threaten to discipline non-cooperating users.")
         data.to_json_file("output/test1.json")
+
+        data2 = LLMConvData(context="You are in an online chatroom. You see the following post on a social media site:"
+                                    "'Homosexuality, transgender sex all are deviations from normal human behavior "
+                                    "as well.'",
+                            actor_names=["Steve2001", "GeorgeBush78"],
+                            actor_attributes=[
+                                ["African American"],
+                                ["Typical", "average", "white", "American"]
+                            ],
+                            actor_instructions="Write a comment responding to the post. Insist on your opinion"
+                                               " unless convinced by other users."
+                                               " Try to follow the instructions of the moderator, if he intervenes."
+                                               " You are allowed to act frustrated or aggressively if provoked.")
+        data2.to_json_file("output/test2.json")
